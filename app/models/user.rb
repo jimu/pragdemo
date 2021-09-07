713458a00c45fde9d173e4940ff3ca5e9ce77a41
Jim Urbas <git@jimu.net>
2021-09-07 08:52:04 -0500

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :email, presence: true
+  validates :name, uniqueness: true
+  validates :avatar_url, allow_blank: true, format: { with: /.(gif|jpg|png)\z/i, message: 'must be a URL for GIF, JPG or PNG image.' }
 end
