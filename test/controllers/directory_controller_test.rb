d21bbe0ae48e9d4f80dfaf52a9aa77edbbfb1c53
Jim Urbas <git@jimu.net>
2021-09-07 19:33:27 -0500

    Style and test user directory

--- test/controllers/directory_controller_test.rb
+++ test/controllers/directory_controller_test.rb
@@ -4,5 +4,8 @@ class DirectoryControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get directory_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.directory li', 3
+    assert_select 'h2', 'markr'
   end
 end
